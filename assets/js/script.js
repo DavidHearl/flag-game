4df1f3813fc49cb9706e882feb2dda13fbaac33e
DavidHearl <david.hearl@outlook.com>
2021-10-21 11:07:50 +0000

    Added javascript to test connection, changed answer divs to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let flags = [
+    'afghanistan',
+    'aland-isladnd',
+    'albania',
+    'algeria',
+    'american-samoa'
+]
+
+
+document.getElementById("answer1").innerText = flags[0]
+document.getElementById("answer2").innerText = flags[1]
+document.getElementById("answer3").innerText = flags[2]
+document.getElementById("answer4").innerText = flags[3]
+
