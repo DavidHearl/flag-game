46e2acd321b65ea7b46dd9d7c133860e6e40c619
DavidHearl <david.hearl@outlook.com>
2021-10-21 11:42:51 +0000

    Added random change flag function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 let flags = [
     'afghanistan',
-    'aland-isladnd',
+    'aland-islands',
     'albania',
     'algeria',
-    'american-samoa'
+    'american-samoa',
+    'andorra'
 ]
 
+document.addEventListener("DOMContentLoaded", function(){
+    runGame()
+})
 
-document.getElementById("answer1").innerText = flags[0]
-document.getElementById("answer2").innerText = flags[1]
-document.getElementById("answer3").innerText = flags[2]
-document.getElementById("answer4").innerText = flags[3]
+function runGame() {
+    let randomFlag = Math.floor(Math.random() * flags.length)
+    document.getElementById("flag-image").src = "assets/images/flags/" + flags[randomFlag] + ".png"
+
+    let num1 = Math.floor(Math.random() * flags.length);
+    let num2 = Math.floor(Math.random() * flags.length);
+    let num3 = Math.floor(Math.random() * flags.length);
+    let num4 = Math.floor(Math.random() * flags.length);
+
+    document.getElementById("answer1").innerText = flags[num1];
+    document.getElementById("answer2").innerText = flags[num2];
+    document.getElementById("answer3").innerText = flags[num3];
+    document.getElementById("answer4").innerText = flags[num4];
+
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+}
 
