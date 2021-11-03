79f0ada353be8f441f34166a1c28164886572371
DavidHearl <david.hearl@outlook.com>
2021-11-03 15:28:39 +0000

    Deleted the second page (will become a popup menu). Fixed bugs and got game working again. Used commit 27 as reference

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 // Run game on page load
-//document.addEventListener("DOMContentLoaded", function(){
-//    setupGame()
-//    runTimer()
-//    runGame()
-//})
+document.addEventListener("DOMContentLoaded", function(){
+    setupGame()
+    //runTimer()
+    runGame()
+})
+
+let trigger = document.getElementById("select-difficulty");
+let menu = document.getElementById("difficulty-menu");
+
+trigger.addEventListener("click", function() {
+    if (menu.style.opacity = 1) {
+        menu.style.opacity = 0.1;
+    }
+})
 
 var correctAnswer;
 var gameFlags = []
-var numberOfQuestions;
 let gameNumber = 0
-
+//var numberOfQuestions;
 
 /**
  * Creates an array of 'x' random flags based on difficulty level selected
@@ -23,7 +31,7 @@ function setupGame() {
         [flags[currentIndex],flags[randomIndex]] = [flags[randomIndex], flags[currentIndex]];
     }
     // Pushes random flags to the array
-    for (i = 0; i<numberOfQuestions; i++) {
+    for (i = 0; i<30; i++) {
         gameFlags.push({country: flags[i].country, image: flags[i].image})
     }
     console.log(gameFlags)
@@ -31,29 +39,55 @@ function setupGame() {
 }
 
 function runGame() {
-    //for (i=0; i<5; i++) {
-    //document.getElementsByClassName("user-choice")[i].style.background = "white";
-    //}
+    for (i=0; i<5; i++) {
+    document.getElementsByClassName("user-choice")[i].style.background = "white";
+    }
     
-    //var imageLocation = "assets/images/flags/" + gameFlags[gameNumber].image + ".png"
-    //document.getElementById("flag-image").src = imageLocation
+    var imageLocation = "assets/images/flags/" + gameFlags[gameNumber].image + ".png"
+    document.getElementById("flag-image").src = imageLocation
 
     // Assigns random values to user-choice from the flag array
-    //for (i=0; i<5; i++) {
-    //    document.getElementsByClassName("user-choice")[i].innerText = flags[Math.floor(Math.random() * flags.length)].country;
-    //}
+    for (i=0; i<5; i++) {
+        document.getElementsByClassName("user-choice")[i].innerText = flags[Math.floor(Math.random() * flags.length)].country;
+    }
 
     // Assigns the correct answer to a random use choice box
-    //let randomSelection = Math.floor(Math.random() * 5)
-    //document.getElementsByClassName("user-choice")[randomSelection].innerText = gameFlags[gameNumber].country
+    let randomSelection = Math.floor(Math.random() * 5)
+    document.getElementsByClassName("user-choice")[randomSelection].innerText = gameFlags[gameNumber].country
 
     // Sets the correct answer
-    //correctAnswer = gameFlags[gameNumber].country;
-    //document.getElementById("progress-bar").style.width = (gameNumber/29)*100 + "%"
-    //gameNumber++
-    //console.log((gameNumber/30)*100)
-    //console.log(correctAnswer)
+    correctAnswer = gameFlags[gameNumber].country;
+    document.getElementById("progress-bar").style.width = (gameNumber/29)*100 + "%"
+    gameNumber++
+    console.log((gameNumber/30)*100)
+    console.log(correctAnswer)
 }
+
+
+
+// Turns difficulty boxes on home page opaique, signaling selection of difficulty.
+//let hover = document.getElementsByClassName("difficulty-box");
+
+//hover[0].addEventListener("click", event => {
+//    numberOfQuestions = 10
+//    setupGame()
+//})
+//hover[1].addEventListener("click", event => {
+//    numberOfQuestions = 20
+//    setupGame()
+//})
+//hover[2].addEventListener("click", event => {
+//    numberOfQuestions = 30
+//    setupGame()
+//})
+//hover[3].addEventListener("click", event => {
+//    numberOfQuestions = 50
+//   setupGame()
+//})
+
+
+
+
 
 
 function runTimer() {
